5b48f2338255668e29532cb721195d8f57ff6b50
Simrann Arora <simrann2099@gmail.com>
2019-07-01 00:52:22 +0530

    Binary search trees ( path sum from root to leaf)

---
+++ BST- path sum root to leaf.cpp
@@ -0,0 +1,109 @@
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+#include<string>
+void helper(BinaryTreeNode<int> *root, int k, int a[],int s) {
+   
+  if(root==NULL||k<0)
+  return;
+ if(root->left==NULL&&root->right==NULL&&root->data==k)
+  {
+   s++;
+   a[s-1]=root->data;
+   for( int i=0;i<s;i++)
+     cout<<a[i]<<" ";
+   cout<<'\n';
+  }
+  int i,temp[1000];
+  for(i=0;i<s;i++)
+  
+    temp[i]=a[i];
+  
+  temp[i]=root->data;
+  s++;
+   helper(root->left,  k-root->data,temp, s) ;
+   helper(root->right, k-root->data ,temp,s) ;
+
+  
+}
+void rootToLeafPathsSumToK(BinaryTreeNode<int> *root, int k) 
+{     
+  int a[1000];
+  helper(root,k,a,0);
+   
+}
+
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    ~BinaryTreeNode() {
+    	if(left) 
+       		delete left;
+    	if(right) 
+       		delete right;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int k;
+    cin >> k;
+    rootToLeafPathsSumToK(root, k);
+    delete root;
+}
+
+
