ae86c737299e6b5ab3cec33ac5cf9550f047d78c
Simrann Arora <simrann2099@gmail.com>
2019-07-01 00:58:48 +0530

    update changes

--- BST to LL.cpp
+++ BST to LL.cpp
@@ -50,3 +50,75 @@ Node<int>* constructBST(BinaryTreeNode<int>* root) {
   return a;
  
 }
+
+
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class Node{
+public:
+    T data;
+    Node<T> *next;
+    Node(T data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    Node<int>* head = constructBST(root);
+    while(head != NULL) {
+        cout << head -> data << " ";
+        head = head -> next;
+    }
+    
+}
+
