36ab613ecf51d81ac60068531afe5bd448f93ba1
Simrann Arora <simrann2099@gmail.com>
2019-07-01 00:54:54 +0530

    LCA of BST

---
+++ LCA of BST.cpp
@@ -0,0 +1,94 @@
+// Following is the Binary Tree node structure
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int lcaInBST(BinaryTreeNode<int>* root , int val1 , int val2){
+
+  if(root==NULL)
+    return -1;
+  if(root->data==val1&&root->data==val2)
+    return root->data;
+ int a,b; 
+if(val1>root->data&&val2>root->data)
+{  a= lcaInBST( root->right ,val1 ,val2);
+   return a;
+}
+if(val1<root->data&&val2<root->data)
+{ b= lcaInBST( root->left ,val1 ,val2);
+   return b;
+}
+
+}
+
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+    public : 
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BinaryTreeNode() {
+    	if(left) 
+       		delete left;
+    	if(right) 
+       		delete right;
+    }
+};
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    int a, b;
+    cin >> a >> b;
+    cout << lcaInBST(root, a, b);
+    delete root;
+}
