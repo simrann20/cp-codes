96a0dec187642fc3aa0466c23d262d1b8180a657
Simrann Arora <simrann2099@gmail.com>
2019-07-03 02:02:07 +0530

    Update BST- path sum root to leaf.cpp

--- BST- path sum root to leaf.cpp
+++ BST- path sum root to leaf.cpp
@@ -46,26 +46,6 @@ void rootToLeafPathsSumToK(BinaryTreeNode<int> *root, int k)
    
 }
 
-template <typename T>
-class BinaryTreeNode {
-    public : 
-    T data;
-    BinaryTreeNode<T> *left;
-    BinaryTreeNode<T> *right;
-
-    BinaryTreeNode(T data) {
-        this -> data = data;
-        left = NULL;
-        right = NULL;
-    }
-
-    ~BinaryTreeNode() {
-    	if(left) 
-       		delete left;
-    	if(right) 
-       		delete right;
-    }
-};
 
 BinaryTreeNode<int>* takeInput() {
     int rootData;
