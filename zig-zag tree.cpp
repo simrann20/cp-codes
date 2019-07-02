a39a9d5c5f3b271cd28ae28b8e11bfaf2a331f23
Simrann Arora <simrann2099@gmail.com>
2019-07-03 01:57:16 +0530

    Update zig-zag tree.cpp

--- zig-zag tree.cpp
+++ zig-zag tree.cpp
@@ -35,28 +35,10 @@ cout<<"\n";
     
     
   }
-  cout<<"\n";
-    
-    
+  cout<<"\n"; 
   } 
       
   }
-   
-template <typename T>
-class BinaryTreeNode {
-    public : 
-    T data;
-    BinaryTreeNode<T> *left;
-    BinaryTreeNode<T> *right;
-
-    BinaryTreeNode(T data) {
-        this -> data = data;
-        left = NULL;
-        right = NULL;
-    }
-};
-
- 
 
 template <typename T>
 class BinaryTreeNode {
@@ -71,4 +53,39 @@ class BinaryTreeNode {
         right = NULL;
     }
 };
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    //cout << "Enter root data : ";
+    cin >> rootData;
+    if(rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while(!q.empty()) {
+        BinaryTreeNode<int> *currentNode = q.front();
+	q.pop();
+        int leftChild, rightChild;
+        //cout << "Enter left child of " << currentNode -> data << " : ";
+        cin >> leftChild;
+        if(leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode -> left =leftNode;
+            q.push(leftNode);
+        }
+        //cout << "Enter right child of " << currentNode -> data << " : ";
+        cin >> rightChild;
+        if(rightChild != -1) {
+            BinaryTreeNode<int>* rightNode = new BinaryTreeNode<int>(rightChild);
+            currentNode -> right =rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
 
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    zigZagOrder(root);
+}
