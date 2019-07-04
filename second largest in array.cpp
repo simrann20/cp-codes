7dc155598a2be3a18dd327cf51f82069b7380afb
Simrann Arora <simrann2099@gmail.com>
2019-07-05 00:32:53 +0530

    Update second largest in array.cpp

--- second largest in array.cpp
+++ second largest in array.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-using namespace std;
 #include<climits>
+using namespace std;
 int FindSecondLargest(int arr[], int n){
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * Return output and don't print it.
-     * Taking input and printing output is handled automatically.
-     */
   int i,j,max;
   max=arr[0];
   if(n<=1)
