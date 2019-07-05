157daae23d874f90cbcf7497bf1f467295f21947
Simrann Arora <simrann2099@gmail.com>
2019-07-06 02:41:30 +0530

    Update change-case.cpp

--- change-case.cpp
+++ change-case.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
 using namespace std;
+void convertStringToLowerCase(char input[]) {
+ 
+  int i;
+  for(i=0;input[i]!='\0';i++)
+  {
+    if(input[i]==' ')
+      continue;
+    else
+      input[i]+=32;
+  }
+}
 
 int main() {
     char input[1000];
