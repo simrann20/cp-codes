360b95d73c4a6a2f6338199e3f225820840fa49d
Simrann Arora <simrann2099@gmail.com>
2019-07-07 19:49:29 +0530

    Create Palindrome LL.cpp

---
+++ Palindrome LL.cpp
@@ -0,0 +1,51 @@
+//Check if a linked list is a palindrome
+#include <iostream>
+class node{
+public:
+    int data;
+    node * next;
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+using namespace std;
+#include "solution.h"
+node* takeinput(){
+    int data;
+    cin>>data;
+    node* head=NULL,*tail=NULL;
+    while(data!=-1){
+        node *newnode=new node(data);
+        if(head==NULL)                  {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            tail=newnode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    node*head=takeinput();
+    bool ans=check_palindrome(head);
+    if(ans)
+        cout<<"true";
+    else 
+        cout << "false";
+    return 0;
+}
