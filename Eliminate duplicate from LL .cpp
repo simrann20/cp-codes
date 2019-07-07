e0a972b869880822cf6fff98007ee989b781e985
Simrann Arora <simrann2099@gmail.com>
2019-07-07 19:52:37 +0530

    Create Eliminate duplicate from LL .cpp

---
+++ Eliminate duplicate from LL .cpp
@@ -0,0 +1,48 @@
+//Eliminate duplicates from a sorted linked list
+#include <iostream>
+class node{
+public:
+    int data;
+    node * next;
+    node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+using namespace std;
+#include "solution.h"
+node* takeinput(){
+    int data;
+    cin>>data;
+    node* head=NULL,*tail=NULL;
+    while(data!=-1){
+        node *newnode=new node(data);
+        if(head==NULL)                  {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            tail=newnode;
+        }
+        cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    node* head=takeinput();
+    head=eliminate_duplicate(head);
+    print(head);
+    return 0;
+}
+
