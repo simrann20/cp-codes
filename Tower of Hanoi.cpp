e4dc284758c19dd0f96b0a73116b3afd72e7b6a0
Simrann Arora <simrann2099@gmail.com>
2019-07-07 20:00:32 +0530

    Tower of Hanoi

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-#include "solution.h"
+void towerOfHanoi(int n, char source, char auxiliary, char destination) {
+ 
+  if(n==0)
+    return;
+  towerOfHanoi(n-1, source, destination, auxiliary);
+  cout<<source<<" "<<destination<<'\n';
+  towerOfHanoi(n-1, auxiliary, source, destination);
+}
 
 int main() {
     int n;
