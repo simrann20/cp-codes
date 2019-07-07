183d576016b7ba1d77a2e6a1ead8ecbe3c853e04
Simrann Arora <simrann2099@gmail.com>
2019-07-07 20:10:46 +0530

    Remove X

--- Remove X.cpp
+++ Remove X.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
-#include "solution.h"
 using namespace std;
+
+void removeX(char a[]) {
+    // Write your code here
+ 
+     if(a[0]=='\0')
+       return ;
+  if(a[0]=='x')
+  {     
+    for(int i=0;a[i]!='\0';i++)
+    {
+       
+      a[i]=a[i+1];
+    }
+    removeX(a);
+  }                                                                      
+  removeX(a+1);
+}
 
 int main() {
     char input[100];
