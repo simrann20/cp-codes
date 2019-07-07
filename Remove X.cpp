fad5484e5d4f5d65a0b688acaa1c4ed67eccf69c
Simrann Arora <simrann2099@gmail.com>
2019-07-07 20:09:07 +0530

    Create Remove X.cpp

---
+++ Remove X.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+int main() {
+    char input[100];
+    cin.getline(input, 100);
+    removeX(input);
+    cout << input << endl;
+}
