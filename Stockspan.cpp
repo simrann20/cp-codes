665706f6f143ac6c9e4065534e4bbaab34443b26
Simrann Arora <simrann2099@gmail.com>
2019-07-09 13:07:39 +0530

    Create Stockspan.cpp

---
+++ Stockspan.cpp
@@ -0,0 +1,61 @@
+#include<stack>
+#include <iostream>
+using namespace std;
+#include "solution.h"
+
+int main() {
+    int size;
+    cin >> size;
+
+    int *input = new int[size];
+    for(int i = 0; i < size; i++) {
+    	cin >> input[i];
+    }
+    int *output = stockSpan(input, size);
+    for(int i = 0; i < size; i++) {
+    	cout << output[i] << " ";
+    }
+}
+
+int* stockSpan(int *price, int size) {
+	// Write your code here
+ int *out =new int [size];
+  out[0]=1;
+  
+         stack<int> s;
+
+  s.push(0);
+  for(int i=1;i<size;i++)
+  {
+    if(price[i]<=price[s.top()])
+     {  out[i]=i-s.top();
+             s.push(i);  
+     }
+     else {
+       while(!s.empty()&&price[s.top()]<price[i])
+       s.pop();
+     
+    if(s.empty()) {
+      out[i]=i+1;
+    }
+    else {
+      out[i]=i-s.top();
+    }
+    s.push(i);
+     }
+  }
+   return out;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
