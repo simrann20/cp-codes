e2540815b72a50038b10508481d934b855a213c2
Simrann Arora <simrann2099@gmail.com>
2019-07-09 13:05:19 +0530

    Update ReverseQueue.cpp

--- ReverseQueue.cpp
+++ ReverseQueue.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+#include <queue>
 
+void reverseQueue(queue<int> &q) {
+  if(q.size()<=1)
+    return ;
+  int x= q.front();
+  q.pop();
+  reverseQueue(q);
+  q.push(x);
+
+}
 int main() {
     queue<int> q;
     int size;
