600fe02751dbb98d909cd73bebbf4f6ae4286b54
Simrann Arora <simrann2099@gmail.com>
2019-07-14 23:37:09 +0530

    Create HighestFrequencynum.cpp

---
+++ HighestFrequencynum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int input[100000];
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>input[i];
+    }
+    int maxKey= highestFrequency(input, n);
+    cout<<maxKey;
+    return 0;
+}
+
+
