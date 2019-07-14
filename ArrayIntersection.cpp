2dd22d897899a990116282a6ae664ee23f955f44
Simrann Arora <simrann2099@gmail.com>
2019-07-14 23:29:48 +0530

    Update ArrayIntersection.cpp

--- ArrayIntersection.cpp
+++ ArrayIntersection.cpp
@@ -8,37 +8,22 @@ using namespace std;
 // size1 - size of first array
 // size2 - size of second array
 void intersection(int input1[], int input2[], int size1, int size2) {
-    /* Don't write main().
-     * Don't read input, it is passed as function argument.
-     * Print the output and don't return it.
-     * Taking input is handled automatically.
-     */
      unordered_map<int,int>m;
   int i;
   for( i=0;i<size1;i++)
   {
     int key=input1[i];
-    
-      m[key]++;
-    
+      m[key]++;   
   }
- 
-  
   for(int j=0;j<size2;j++)
   {
     if(m[input2[j]]>0)
     {
-      
         cout<<input2[j]<<'\n';
         m[input2[j]]--;
-        
-     
-    }
-  
-  
+    } 
 }
        }
-
 int main() {
 
 	int size1,size2;
