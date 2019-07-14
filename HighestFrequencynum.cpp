feeed284085c7e03e5dc8665072e188820f5b26e
Simrann Arora <simrann2099@gmail.com>
2019-07-14 23:37:34 +0530

    Update HighestFrequencynum.cpp

--- HighestFrequencynum.cpp
+++ HighestFrequencynum.cpp
@@ -1,5 +1,38 @@
 #include<iostream>
 using namespace std;
+#include<climits>
+#include<unordered_map>
+	int highestFrequency(int *input, int n){	 
+		
+	/* Don't write main().
+     	* the input array is already passed as function argument.
+     	* Taking input and printing output is handled automatically.
+     	*/
+      int key;
+      unordered_map<int,int>m;
+      for(int i=0;i<n;i++)
+      {    key=input[i];
+       // to check if that key is present before
+         if(m.count(key)==0)
+           m[key]=1;
+         else
+             m[key]++;
+   
+      
+      }
+     	int max=INT_MIN;
+      int d;
+      
+      for(int i=0;i<n;i++)
+      {
+        if(max<m[input[i]])
+        { max=m[input[i]];
+        d=input[i];
+      }
+      }
+     	return d;
+ 	}
+
 
 int main()
 {
