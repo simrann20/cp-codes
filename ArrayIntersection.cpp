38188c46ae6965b24bb83abc7a924c9b2d0711d5
Simrann Arora <simrann2099@gmail.com>
2019-07-14 23:27:25 +0530

    Create ArrayIntersection.cpp

---
+++ ArrayIntersection.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main() {
+
+	int size1,size2;
+
+	cin>>size1;
+	int *input1=new int[1+size1];	
+	
+	for(int i=0;i<size1;i++)
+		cin>>input1[i];
+
+	cin>>size2;
+	int *input2=new int[1+size2];	
+	
+	for(int i=0;i<size2;i++)
+		cin>>input2[i];
+	
+	
+	intersection(input1,input2,size1,size2);
+
+		
+	return 0;
+}
